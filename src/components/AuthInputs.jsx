ca716d1e193ff69e82a0d3c72a1dd37b10d3ad0f
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-19 23:06:33 +0100

    conditional styling/ dynamic styling

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,20 +24,21 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
 
             // Adding dynamic and conditional styling when using in line styling
-            style={{
-              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            }}
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            // }}
+
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
