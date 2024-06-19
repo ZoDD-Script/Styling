de133a9b9a0157cf1a923e9cefe8c75724faa7fc
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-19 23:00:55 +0100

    Adding dynamic and conditional styling when using in line styling

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -27,6 +27,11 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
+
+            // Adding dynamic and conditional styling when using in line styling
+            style={{
+              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            }}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
