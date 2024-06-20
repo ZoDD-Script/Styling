6e96cc3a4a09983d36954d15af6048be732838ab
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-20 13:49:35 +0100

    creating reuseable components/ styled-components combination

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,48 +1,13 @@
 import { useState } from 'react';
-import { styled } from 'styled-components'
+import { styled } from 'styled-components';
+import Button from './Button';
+import Input from './Input';
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  // color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'};
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover{
-    background-color: #f0920e;
-  }
 `
 
 export default function AuthInputs() {
@@ -68,27 +33,23 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}
-            // className={`label paragraph ${emailNotValid ? 'invalid' : ''}`}
-          >
-            Email
-          </Label>
-          <Input
-            type="email"
-
-            // Adding dynamic and conditional styling when using in line styling
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
-
-            // className={emailNotValid ? 'invalid' : undefined}
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}
+        <Input
+          label='Email'
+          type='email'
+          invalid={emailNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
+        <Input
+          label='Password'
+          type='password'
+          invalid={emailNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
+          {/* <Label $invalid={passwordNotValid}
             // className={`label ${passwordNotValid ? 'invalid' : ''}`}
           >Password</Label>
           <Input
@@ -99,7 +60,7 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
+        </Input> */}
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
