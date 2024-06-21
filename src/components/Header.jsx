8c0751bc94a843e45f0ee16a73d9d98643c1e807
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-21 21:55:36 +0100

    dealing with background color/image and font with tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,7 +72,7 @@ export default function Header() {
   return (
     <header className='flex flex-col items-center mt-8 mb-16'>
       <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain' />
-      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800'>ReactArt</h1>
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title'>ReactArt</h1>
 
       {/* // Inline style */}
       {/* <p style={{
@@ -80,7 +80,7 @@ export default function Header() {
         textAlign: 'left'
       }}>A community of artists and art-lovers.</p> */}
 
-      <p>A community of artists and art-lovers.</p>
+      <p className='text-stone-500'>A community of artists and art-lovers.</p>
     </header>
   );
 }
